5bc3036c63fc4c658f80e4b6920494caf70cf432
Joel Marcotte <joel.marcotte@gmail.com>
2020-03-10 09:27:43 -0400

    feat: basic cases, and test todos
    * working through thought model
    
    * visitor
    
    * passing test
    
    * more tests for base cases
    
    * test cleanup
    
    * can mock interfaces
    
    * bunch of test todos
    
    * cleanup
    
    * fix tsc
    
    * fix coverage
    
    Co-authored-by: Joel Marcotte <joelm@surveymonkey.com>

--- src/__tests__/lib.test.ts
+++ src/__tests__/lib.test.ts
@@ -1,5 +1,276 @@
-import lib from "..";
+import { mock } from "..";
+import { buildSchemaFromTypeDefinitions } from "graphql-tools";
+import { visitWithTypeInfo } from "graphql";
+// import { graphql, GraphQLResolveInfo } from "graphql";
 
-test("it returns true", () => {
-  expect(lib()).toBe(true);
+const schemaSDL = /* GraphQL */ `
+  scalar MissingMockType
+  interface Flying {
+    id: String!
+    returnInt: Int
+  }
+  type Bird implements Flying {
+    id: String!
+    returnInt: Int
+    returnString: String
+    returnStringArgument(s: String): String
+  }
+  type Bee implements Flying {
+    id: String!
+    returnInt: Int
+    returnEnum: SomeEnum
+  }
+  union BirdsAndBees = Bird | Bee
+  enum SomeEnum {
+    A
+    B
+    C
+  }
+  type RootQuery {
+    returnInt: Int
+    returnFloat: Float
+    returnString: String
+    returnBoolean: Boolean
+    returnID: ID
+    returnEnum: SomeEnum
+    returnBirdsAndBees: [BirdsAndBees]
+    returnFlying: [Flying]
+    returnMockError: MissingMockType
+    returnNullableString: String
+    returnNonNullString: String!
+    returnObject: Bird
+    returnListOfInt: [Int]
+    returnListOfIntArg(l: Int): [Int]
+    returnListOfListOfInt: [[Int!]!]!
+    returnListOfListOfIntArg(l: Int): [[Int]]
+    returnListOfListOfObject: [[Bird!]]!
+    returnStringArgument(s: String): String
+    node(id: String!): Flying
+    node2(id: String!): BirdsAndBees
+  }
+  type RootMutation {
+    returnStringArgument(s: String): String
+  }
+  schema {
+    query: RootQuery
+    mutation: RootMutation
+  }
+`;
+
+const schema = buildSchemaFromTypeDefinitions(schemaSDL);
+
+describe("Automocking", () => {
+  describe("Guardrails", () => {
+    test.todo("it throws without a schema");
+    test.todo("it throws without a valid schema");
+    test.todo("it throws without a query");
+    test.todo("it throws without a valid query");
+  });
+
+  describe("No provided mocks", () => {
+    test("automocks the default types automatically", () => {
+      const testQuery = /* GraphQL */ `
+        {
+          returnInt
+          returnString
+          returnFloat
+          returnBoolean
+          returnID
+        }
+      `;
+      const resp: any = mock(schema, testQuery);
+      expect(resp.data).toMatchObject({
+        returnInt: expect.toBeNumber(),
+        returnString: expect.toBeString(),
+        returnFloat: expect.toBeNumber(),
+        returnBoolean: expect.toBeBoolean(),
+        returnID: expect.toBeString()
+      });
+      expect(resp.data.returnInt % 1 === 0).toBe(true);
+      expect(resp.data.returnFloat % 1 !== 0).toBe(true);
+    });
+
+    test("can automock enums", () => {
+      const testQuery = /* GraphQL */ `
+        {
+          returnEnum
+        }
+      `;
+      const resp: any = mock(schema, testQuery);
+      expect(resp.data).toMatchObject({
+        returnEnum: expect.toBeOneOf(["A", "B", "C"])
+      });
+    });
+
+    test("can automock unions", () => {
+      const testQuery = /* GraphQL */ `
+        {
+          returnBirdsAndBees {
+            __typename
+            ... on Bird {
+              returnInt
+              returnString
+            }
+            ... on Bee {
+              returnInt
+              returnEnum
+            }
+          }
+        }
+      `;
+      const resp: any = mock(schema, testQuery);
+      expect(resp.data.returnBirdsAndBees.length).toBeGreaterThan(0);
+      expect(resp.data.returnBirdsAndBees.length).toBeLessThan(5);
+      const firstType = resp.data.returnBirdsAndBees[0];
+      expect(firstType.returnInt).toBeNumber();
+      if (firstType.__typename === "Bird") {
+        expect(firstType.returnString).toBeString();
+      } else {
+        expect(firstType.returnEnum).toBeOneOf(["A", "B", "C"]);
+      }
+    });
+
+    test("can automock interfaces", () => {
+      const testQuery = /* GraphQL */ `
+        {
+          returnFlying {
+            __typename
+            ... on Bird {
+              returnInt
+              returnString
+            }
+            ... on Bee {
+              returnInt
+              returnEnum
+            }
+          }
+        }
+      `;
+      const resp: any = mock(schema, testQuery);
+      expect(resp.data.returnFlying.length).toBeGreaterThan(0);
+      expect(resp.data.returnFlying.length).toBeLessThan(5);
+      const firstType = resp.data.returnFlying[0];
+      expect(firstType.returnInt).toBeNumber();
+      if (firstType.__typename === "Bird") {
+        expect(firstType.returnString).toBeString();
+      } else {
+        expect(firstType.returnEnum).toBeOneOf(["A", "B", "C"]);
+      }
+    });
+
+    test.todo("automocking of lists are deterministic on some seed");
+
+    test.todo("can automock objects");
+
+    test.todo("can automock nested unions");
+    test.todo("can automock nested interfaces");
+    test.todo("can automock nested basic types");
+    test.todo("can automock nested enums");
+    test.todo("can automock nested objects");
+    test.todo("can automock inline fragments");
+
+    test.todo("can provide field mock override");
+  });
+
+  describe("With partial mocks provided", () => {
+    test("can return string mock", () => {
+      // Given a query
+      const query = /* GraphQL */ `
+        query SampleQuery {
+          returnInt
+          returnString
+        }
+      `;
+
+      // And a partial mock
+      const mocks = { returnString: "bar" };
+      const resp: any = mock(schema, query, mocks);
+
+      // Return a fully mocked response
+      expect(resp).toMatchObject({
+        data: {
+          returnInt: expect.toBeNumber(),
+          returnString: "bar"
+        }
+      });
+    });
+
+    test.todo("can return basic types in mock");
+    test.todo("can return basic types list in mock");
+    test.todo("can return basic enum mock");
+    test.todo("can return basic enum list mock");
+    test.todo("throws if provided enum mock is invalid");
+    test.todo("can return object mock");
+    test.todo("can return object mock list");
+    test.todo("can return union mock list");
+    test.todo("can return interface mock list");
+    test.todo("can return partial inline fragment mock");
+    test.todo("can return provided nested basic types");
+    test.todo("can return provided nested basic types list");
+    test.todo("can return provided nested enums");
+    test.todo("can return provided nested enums list");
+    test.todo("can return provided nested unions");
+    test.todo("can return provided nested interfaces");
+    test.todo("executes functions when provided, with variables as args");
+  });
+
+  describe("mocking errors", () => {
+    test.todo("can provide Errors for basic types");
+    test.todo("can provide Errors for enums");
+    test.todo("can provide Errors for objects");
+    test.todo("can provide Errors for lists (one error among list items)");
+    test.todo("can provide Errors for unions");
+    test.todo("can provide Errors for interfaces");
+    test.todo("can throw errors in functions as resolver");
+  });
+
+  test("base case - TBD remove this test later", () => {
+    // Given a query
+    const query = /* GraphQL */ `
+      query SampleQuery {
+        returnInt
+        returnString
+        returnFlying {
+          __typename
+          id
+          ... on Bird {
+            returnString
+          }
+          ... on Bee {
+            returnEnum
+          }
+          returnInt
+        }
+      }
+    `;
+
+    // And a partial mock
+    const mocks = {
+      returnString: "bar",
+      returnFlying: [
+        { __typename: "Bee", id: "123" },
+        { __typename: "Bee" },
+        { __typename: "Bird" }
+      ]
+    };
+
+    const resp: any = mock(schema, query, mocks);
+
+    // Return a fully mocked response
+    expect(resp).toMatchObject({
+      data: {
+        returnInt: expect.toBeNumber(),
+        returnString: "bar",
+        returnFlying: [
+          { __typename: "Bee", id: "123", returnEnum: expect.toBeOneOf(["A", "B", "C"]) },
+          {
+            __typename: "Bee",
+            id: expect.toBeString(),
+            returnEnum: expect.toBeOneOf(["A", "B", "C"])
+          },
+          { __typename: "Bird", id: expect.toBeString(), returnString: expect.toBeString() }
+        ]
+      }
+    });
+  });
 });
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,193 @@
-export default function main() {
-  return true;
+import {
+  GraphQLSchema,
+  parse,
+  execute,
+  DocumentNode,
+  visitWithTypeInfo,
+  TypeInfo,
+  GraphQLType,
+  FieldNode,
+  GraphQLObjectType,
+  visit,
+  Kind,
+  GraphQLInterfaceType,
+  GraphQLUnionType,
+  getNamedType,
+  GraphQLField,
+  getNullableType,
+  GraphQLNullableType,
+  GraphQLResolveInfo,
+  GraphQLFieldResolver,
+  GraphQLList,
+  GraphQLEnumType,
+  isAbstractType,
+  isObjectType
+} from "graphql";
+import { getFieldDef } from "graphql/execution/execute";
+
+type IteratorFn = (fieldDef: GraphQLField<any, any>, parentType: string, fieldName: string) => void;
+
+function forEachFieldInQuery(schema: GraphQLSchema, document: DocumentNode, fn: IteratorFn) {
+  const typeInfo = new TypeInfo(schema);
+  visit(
+    document,
+    visitWithTypeInfo(typeInfo, {
+      [Kind.FIELD](node: FieldNode): FieldNode | null | undefined {
+        const fieldName = node.name.value;
+        if (fieldName === "__typename") {
+          return;
+        }
+        const parentType = typeInfo.getParentType();
+        // const fieldType = typeInfo.getType(); // the return type of this field.
+        if (isAbstractType(parentType)) {
+          const possibleTypes = schema.getPossibleTypes(parentType);
+          possibleTypes.forEach(t => {
+            const fieldDef = getFieldDef(schema, t, fieldName);
+            if (fieldDef) {
+              fn(fieldDef, t.name, fieldName);
+            }
+          });
+        }
+        if (isObjectType(parentType)) {
+          const parentFields = parentType.getFields();
+          const fieldDef = parentFields[node.name.value]; // the schame field definition
+          fn(fieldDef, parentType.name, fieldName);
+        }
+      }
+    })
+  );
+}
+
+function assignResolveType(type: GraphQLType) {
+  const fieldType = getNullableType(type) as GraphQLNullableType;
+  const namedFieldType = getNamedType(fieldType);
+
+  if (
+    namedFieldType instanceof GraphQLUnionType ||
+    namedFieldType instanceof GraphQLInterfaceType
+  ) {
+    // the default `resolveType` always returns null. We add a fallback
+    // resolution that works with how unions and interface are mocked
+    namedFieldType.resolveType = (data: any, context: any, info: GraphQLResolveInfo) => {
+      return info.schema.getType(data.__typename) as GraphQLObjectType;
+    };
+  }
+}
+
+function getRandomElement(ary: ReadonlyArray<any>) {
+  const sample = Math.floor(Math.random() * ary.length);
+  return ary[sample];
+}
+
+const defaultMockMap: Map<string, GraphQLFieldResolver<any, any>> = new Map();
+defaultMockMap.set("Int", () => Math.round(Math.random() * 200) - 100);
+defaultMockMap.set("Float", () => Math.random() * 200 - 100);
+defaultMockMap.set("String", () => "Hello World");
+defaultMockMap.set("Boolean", () => Math.random() > 0.5);
+defaultMockMap.set("ID", () => "123456");
+
+export function mock(schema: GraphQLSchema, query: string, partialMock?: any, options?: any) {
+  const document = parse(query);
+
+  const mockResolverFunction = function(type: GraphQLType, typeName?: string, fieldName?: string) {
+    // order of precendence for mocking:
+    // 1. if the object passed in already has fieldName, just use that
+    // --> if it's a function, that becomes your resolver
+    // --> if it's a value, the mock resolver will return that
+    // 2. if the nullableType is a list, recurse
+    // 2. if there's a mock defined for this typeName, that will be used
+    // 3. if there's no mock defined, use the default mocks for this type
+    return (
+      root: any,
+      args: { [key: string]: any },
+      context: any,
+      info: GraphQLResolveInfo
+    ): any => {
+      // nullability doesn't matter for the purpose of mocking.
+      const fieldType = getNullableType(type) as GraphQLNullableType;
+      const namedFieldType = getNamedType(fieldType);
+
+      if (root && fieldName && typeof root[fieldName] !== "undefined") {
+        // // if we're here, the field is already defined on the root object so use it
+        // if (typeof root[fieldName] === "function") {
+        //   result = root[fieldName](root, args, context, info);
+        // } else {
+        //   result = root[fieldName];
+        // }
+
+        return root[fieldName];
+      }
+
+      // Default mock for enums
+      if (fieldType instanceof GraphQLEnumType) {
+        return getRandomElement(fieldType.getValues()).value;
+      }
+
+      // Lists
+      if (fieldType instanceof GraphQLList) {
+        return [
+          mockResolverFunction(fieldType.ofType)(root, args, context, info),
+          mockResolverFunction(fieldType.ofType)(root, args, context, info)
+        ];
+      }
+
+      // Unions and interfaces
+      if (fieldType instanceof GraphQLUnionType || fieldType instanceof GraphQLInterfaceType) {
+        let implementationType;
+        const possibleTypes = schema.getPossibleTypes(fieldType);
+        implementationType = getRandomElement(possibleTypes);
+        return Object.assign(
+          { __typename: implementationType },
+          mockResolverFunction(implementationType)(root, args, context, info)
+        );
+      }
+
+      // Mock default scalars
+      if (defaultMockMap.has(fieldType.name)) {
+        return defaultMockMap.get(fieldType.name)!(root, args, context, info);
+      }
+    };
+  };
+
+  forEachFieldInQuery(schema, document, (field, typeName, fieldName) => {
+    assignResolveType(field.type); // assign the default .resolveType resolver.
+    let mockResolver: GraphQLFieldResolver<any, any>;
+
+    // we have to handle the root mutation and root query types differently,
+    // because no resolver is called at the root.
+    /* istanbul ignore next: Must provide schema DefinitionNode with query type or a type named Query. */
+    const isOnQueryType: boolean = !!(
+      schema.getQueryType() && schema.getQueryType()?.name === typeName
+    );
+    const isOnMutationType: boolean = !!(
+      schema.getMutationType() && schema.getMutationType()?.name === typeName
+    );
+
+    if (isOnQueryType || isOnMutationType) {
+      mockResolver = (
+        root: any,
+        args: { [key: string]: any },
+        context: any,
+        info: GraphQLResolveInfo
+      ) => {
+        return mockResolverFunction(field.type, typeName, fieldName)(
+          partialMock,
+          args,
+          context,
+          info
+        );
+      };
+    } else {
+      mockResolver = mockResolverFunction(field.type, typeName, fieldName);
+    }
+    field.resolve = mockResolver;
+  });
+
+  const resp = execute({
+    schema,
+    document,
+    rootValue: {},
+    contextValue: {}
+  });
+  return resp;
 }
